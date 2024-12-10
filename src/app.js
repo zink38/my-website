afff4086cb8c79f41f9ad130aa3e64f12334506b
Keegan Churchill <KeeganBChurchill@gmail.com>
2024-12-10 19:52:43 -0330

    created a public folder, now serving root with expressJS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/',(req, res)   => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "pages/home/index.html"));
 });
 
 const port = process.env.PORT || 3000;
