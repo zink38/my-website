adb84600d8e608c057d1dc78f3c3568c36f84675
Keegan Churchill <KeeganBChurchill@gmail.com>
2024-12-24 18:06:49 -0330

    experimenting with vercel

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import app from './index.js';
 
 // Start the server
 const port = config.express.port;
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
-export default app;
+// export default app;
