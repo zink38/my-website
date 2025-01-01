77887c54f3e392ab2d10334c58a712ce8f611499
Keegan Churchill <KeeganBChurchill@gmail.com>
2025-01-01 15:24:15 -0330

    custom bootstrap carousel styling for light and dark. Created contact me emailer.

--- src/home/home-controller.ts
+++ src/home/home-controller.ts
@@ -1,18 +1,62 @@
-import { Request, Response, NextFunction } from 'express';
-import { JobModel } from '../jobs/job-model.js';
+import config from "../config.js";
+import { Request, Response, NextFunction } from "express";
+import { JobModel } from "../jobs/job-model.js";
+import nodemailer from "nodemailer";
 
+const transporter = nodemailer.createTransport({
+  service: config.email.service,
+  host: config.email.host,
+  port: config.email.port,
+  secure: config.email.secure,
+  auth: {
+    user: config.email.auth.user,
+    pass: config.email.auth.pass,
+  },
+});
 
 export class HomeController {
-    static async getHomePage(req: Request, res: Response, next: NextFunction) {
-        try {
-            const jobs = await JobModel.getAllJobs(); 
-            res.render('home', { jobs });
-        } catch (error) {
-            // Log the error
-            console.error('Failed to fetch jobs:', error);
-            // Optionally, you can pass an error message to the view
-            // jobs = null; // Indicate that jobs could not be fetched
-        }
-        
+  static async getHomePage(req: Request, res: Response, next: NextFunction) {
+    try {
+      const jobs = await JobModel.getAllJobs();
+      res.render("home", { jobs });
+    } catch (error) {
+      // Log the error
+      console.error("Failed to fetch jobs:", error);
+      // Optionally, you can pass an error message to the view
+      // jobs = null; // Indicate that jobs could not be fetched
     }
+  }
+  // Method for handling job applications
+  static async contactMe(req: Request, res: Response, next: NextFunction) {
+    const { name, email, phone, message, terms } = req.body;
+
+    if (!name || !email || !terms) {
+      res.status(400).send("Missing Required Feilds");
+    } else {
+      try {
+        const mailOptions = {
+          from: config.email.auth.user,
+          to: config.email.auth.user,
+          subject: `My-Website Contact Me`,
+          html: `
+                            <p><strong>Name:</strong> ${name}</p>
+                            <p><strong>Email:</strong> ${email}</p>
+                            <p><strong>Phone:</strong> ${phone}</p>
+                            <p><strong>Message:</strong> ${message}</p>
+                             <p><strong>Terms:</strong> ${terms}</p>
+                        `,
+        };
+
+        transporter.sendMail(mailOptions, (err) => {
+          if (err) {
+            next(err);
+          } else {
+            res.status(200).render("contact-status");
+          }
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  }
 }
--- src/home/home-router.ts
+++ src/home/home-router.ts
@@ -4,5 +4,6 @@ import { HomeController } from './home-controller.js';
 const homeRouter = Router();
 
 homeRouter.get('/', HomeController.getHomePage);
+homeRouter.post('/contact', HomeController.contactMe);
 
 export default homeRouter;
