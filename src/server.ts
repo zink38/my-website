7e352ee81f87d1ed94ae8e6c9822c02da5251059
Keegan Churchill <KeeganBChurchill@gmail.com>
2025-01-05 18:04:26 -0330

    exporting for vercel (forgot to stage)

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import app from './index.js';
 
 
 // Start the server
-const port = config.express.port;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// const port = config.express.port;
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
 
 export default app;
